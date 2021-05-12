c9e29728b6705cd206c88c34fc0426cbe9757dc9
Jeff Hall <jeff.leon.hall@gmail.com>
2021-05-12 09:35:53 -0700

    made a function to make multiple requests for a single number and modified makeAndAppendCards to handle an array

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const $numDiv = $('.num-div');
 
-function getFacts () {
-    let fourFacts = axios.get('http://numbersapi.com/1..8,17?json');
-    fourFacts
-        .then(resp => makeAndAppendCards(resp.data))
-}
 
-function makeAndAppendCards(data) {
-    for (let num in data) {
+
+
+
+function makeAndAppendCards(respArr) {
+    for (let resp of respArr) {
         $numDiv.append(`<div class="card">
                         <div class="card-body">
-                            <h5 class="card-title">${num}</h5>
-                            <h6 class="card-text">${data[num]}</h6>
+                            <h5 class="card-title">${resp.number}</h5>
+                            <h6 class="card-text">${resp.text}</h6>
                         </div>
                      </div>
                      `)
     }
 }
 
-getFacts()
+function makeRequests() {
+    const numFacts = [];
+    for (let i = 0; i < 4; i++) {
+        numFacts.push(axios.get('http://numbersapi.com/23?json'));
+    }
+
+    return numFacts;
+}
